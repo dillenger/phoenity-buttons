78d57419aa743681f4ac8e59dd026f21c7f81d80
dillenger <dillinger@kpnmail.nl>
2021-10-21 18:56:55 +0200

    update

--- defaults/preferences/prefs.js
+++
@@ -1,4 +0,0 @@
-pref("extensions.buttons@dillinger.description", "chrome://phoenitybuttons/locale/phoenityButtons.properties");
-
-pref("extensions.phoenitybuttons.display.headers", false);
-pref("extensions.phoenitybuttons.iconsize.small", true);
